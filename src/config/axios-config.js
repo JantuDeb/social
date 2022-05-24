606590f7764948dfd1601ed1bec0091f94c2d9bb
Jantu Deb <jantud54@gmail.com>
2022-05-25 01:38:21 +0530

    changed api url

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -2,7 +2,7 @@ export const axiosConfig = {
   baseURL:
     window.location.protocol === "http:"
       ? "http://localhost:4000/api/v1/"
-      : "https://social-media-backendapi.herokuapp.com/api/v1/",
+      : "https://api.ionvu.online/api/v1/",
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
